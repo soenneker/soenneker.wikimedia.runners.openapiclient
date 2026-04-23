ab53b2b871b8c23aff8494210529f6a9fd415a62
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-23 10:46:05 -0500

    tunit

--- test/Soenneker.Wikimedia.Runners.OpenApiClient.Tests/WikimediaOpenApiClientRunnerTests.cs
+++ test/Soenneker.Wikimedia.Runners.OpenApiClient.Tests/WikimediaOpenApiClientRunnerTests.cs
@@ -1,16 +1,15 @@
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Wikimedia.Runners.OpenApiClient.Tests;
 
-[Collection("Collection")]
-public sealed class WikimediaOpenApiClientRunnerTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class WikimediaOpenApiClientRunnerTests : HostedUnitTest
 {
-    public WikimediaOpenApiClientRunnerTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public WikimediaOpenApiClientRunnerTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
