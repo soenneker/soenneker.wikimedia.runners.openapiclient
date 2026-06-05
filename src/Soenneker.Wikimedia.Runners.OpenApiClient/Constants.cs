920f00e0e8308528bd97ecb918537e2d60cc03a7
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:59 -0500

    xml commenting

--- src/Soenneker.Wikimedia.Runners.OpenApiClient/Constants.cs
+++ src/Soenneker.Wikimedia.Runners.OpenApiClient/Constants.cs
@@ -1,6 +1,12 @@
 namespace Soenneker.Wikimedia.Runners.OpenApiClient;
 
+/// <summary>
+/// Represents the constants.
+/// </summary>
 public static class Constants
 {
+    /// <summary>
+    /// The library.
+    /// </summary>
     public const string Library = "Soenneker.Wikimedia.OpenApiClient";
 }
